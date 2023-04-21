521e37f5d2cacace750663bfc04e3144ad0e5613
rk37946 <126868265+rk37946@users.noreply.github.com>
2023-04-21 15:12:46 +0530

    Add files via upload
    Added create account and generate pass phrase

---
+++ login.js
@@ -0,0 +1,52 @@
+
+const { log } = require('console');
+const express = require('express');
+const nearAPI = require('near-api-js');
+const app = express();
+const path = require("path");
+const homedir = require("os").homedir();
+
+let k ;
+const HELP = `Please run this script in the following format:
+    node app.js CREATOR_ACCOUNT.testnet NEW_ACCOUNT.testnet AMOUNT Then open your localhost and open the port mentioned.
+`;
+app.get('/', (req, res) => {
+    
+  res.send(main());
+});
+
+app.post('/', (req, res) => {
+  res.send(check(k));
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+
+
+async function main(){
+const { connect, KeyPair, keyStores, utils } = nearAPI;
+const CREDENTIALS_DIR = ".near-credentials";
+const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+const config = {
+  keyStore,
+  networkId: "testnet",
+  nodeUrl: "https://rpc.testnet.near.org",
+};
+
+if (process.argv.length !== 5) {
+  console.info(HELP);
+  process.exit(1);
+}
+const { parseSeedPhrase, generateSeedPhrase } = require('near-seed-phrase');
+
+    // to create a seed phrase with its corresponding Keys
+const {seedPhrase, publicKey, secretKey} = generateSeedPhrase()
+    
+console.log(seedPhrase);
+console.log(publicKey);
+console.log(secretKey);
+}
